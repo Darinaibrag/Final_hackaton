7e1d2883923f79aad0a27302eee9e0ed5c8ce2c1
darinaibragimova <darinaibrag@gmail.com>
2023-08-10 13:57:31 +0600

    booking

--- booking/serializers.py
+++ booking/serializers.py
@@ -18,3 +18,26 @@ class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
+
+    def create(self, validated_data):
+        posts_data = validated_data.pop('posts')
+        request = self.context.get('request')
+        user = request.user
+        validated_data['user'] = user
+
+        # Create the Booking object
+        booking = Booking.objects.create(**validated_data)
+
+        # Create associated BookingItem objects
+        for post_data in posts_data:
+            post_data['user'] = user  # Set the user for each BookingItem
+            BookingItem.objects.create(booking=booking, **post_data)
+
+        return booking
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['posts'] = BookingItemSerializer(
+            instance.items.all(), many=True
+        ).data
+        return representation
